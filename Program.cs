0b00f749473c44d73869ad876fb608848859fe62
Kit Yee Hoang Wu <kit.yee.hoang.wu@brightstraining.com>
2024-10-25 15:22:40 +0200

    Added intro and randomnumber

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("test");
-Console.WriteLine("nu ska gamla master heta main");
+﻿Console.WriteLine("Welcome to guess the number game!");
+Console.WriteLine("Guess a number between 1- 100.");
+
+Random ran = new Random();
+int numbers = ran.Next(1,100);
