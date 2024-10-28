d88b9fedb44c03be7146e18f0d5069c693f5603b
Lukas Graff <Lukas.Graff@brightstraining.com>
2024-10-28 09:51:15 +0100

    Added just two lines of outro text

--- Program.cs
+++ Program.cs
@@ -2,3 +2,12 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("test");
 Console.WriteLine("nu ska gamla master heta main");
+
+
+
+
+
+
+
+Console.WriteLine("Thank you for playing our game, we've put a lot of effort into it and really appreciate you.");
+Console.WriteLine("Have a great day!");
